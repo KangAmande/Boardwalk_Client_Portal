025301c00ce6a55a922e86d5224fa4f980646190
Hruthvik Aka Chokshi <000813765@mohawkcollege.ca>
2022-04-01 14:17:06 -0400

    Revert "resolving conflict"
    This reverts commit 406424928da87da02f3ee647e8a1499d13fad028.

--- WebApplication4/ClientApp/src/store/index.ts
+++ WebApplication4/ClientApp/src/store/index.ts
@@ -1,19 +1,11 @@
 import * as Policies from './Policies';
-
-import * as Drivers from './Drivers';
-
 import * as ClientBuildingInfo  from './ClientBuildingInfo';
-
 
 
 // The top-level state object
 export interface ApplicationState {
     Policies: Policies.PoliciesState | undefined;
-
-    Drivers: Drivers.DriversState | undefined;
-
     ClientBuildingInfo: ClientBuildingInfo.ClientBuildingInfoState | undefined;
-
 }
 
 // Whenever an action is dispatched, Redux will update each top-level application state property using
@@ -21,11 +13,7 @@ export interface ApplicationState {
 // acts on the corresponding ApplicationState property type.
 export const reducers = {
     Policies: Policies.reducer,
-
-    Drivers:Drivers.reducer,
-
     ClientBuildingInfo: ClientBuildingInfo.reducer
-
 };
 
 // This type can be used as a hint on action creators so that its 'dispatch' and 'getState' params are
