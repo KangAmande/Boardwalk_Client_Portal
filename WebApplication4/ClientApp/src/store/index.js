406424928da87da02f3ee647e8a1499d13fad028
Hruthvik Aka Chokshi <000813765@mohawkcollege.ca>
2022-04-01 14:07:15 -0400

    resolving conflict

--- WebApplication4/ClientApp/src/store/index.js
+++ WebApplication4/ClientApp/src/store/index.js
@@ -2,11 +2,19 @@
 exports.__esModule = true;
 exports.reducers = void 0;
 var Policies = require("./Policies");
+
+var Drivers = require("./Drivers");
+
 var ClientBuildingInfo = require("./ClientBuildingInfo");
+
 // Whenever an action is dispatched, Redux will update each top-level application state property using
 // the reducer with the matching name. It's important that the names match exactly, and that the reducer
 // acts on the corresponding ApplicationState property type.
 exports.reducers = {
     Policies: Policies.reducer,
+
+    Drivers: Drivers.reducer,
+
     ClientBuildingInfo: ClientBuildingInfo.reducer
+
 };
