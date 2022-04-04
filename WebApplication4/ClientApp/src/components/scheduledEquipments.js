87b6ddb67d22f7320b8e27216444a33401c82a29
Hruthvikk <101742067+Hruthvikk@users.noreply.github.com>
2022-04-04 12:57:46 -0400

    Make Request Link 6 Data Displayed

--- WebApplication4/ClientApp/src/components/scheduledEquipments.js
+++ WebApplication4/ClientApp/src/components/scheduledEquipments.js
@@ -18,6 +18,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var React = require("react");
 var react_redux_1 = require("react-redux");
 var Sidebarmr_1 = require("./Sidebarmr");
+var EquipmentsStore = require("../store/Equipments");
 var Accordion_1 = require("./Accordion");
 var NavMenu_1 = require("./NavMenu");
 var scheduledEquipments = /** @class */ (function (_super) {
@@ -25,20 +26,30 @@ var scheduledEquipments = /** @class */ (function (_super) {
     function scheduledEquipments() {
         return _super !== null && _super.apply(this, arguments) || this;
     }
+    scheduledEquipments.prototype.componentDidMount = function () {
+        this.ensureDataFetched();
+    };
+    // This method is called when the route parameters change
+    scheduledEquipments.prototype.componentDidUpdate = function () {
+        this.ensureDataFetched();
+    };
+    scheduledEquipments.prototype.ensureDataFetched = function () {
+        var startDateIndex = parseInt(this.props.match.params.startDateIndex, 10) || 0;
+        this.props.requestEquipments(startDateIndex);
+    };
+    scheduledEquipments.prototype.showEquipments = function () {
+        console.log(this.props.Equipment);
+        return (React.createElement("div", null, this.props.Equipment.map(function (d, index) {
+            return React.createElement("div", null,
+                React.createElement(Accordion_1.CustomAccordion, { key: index, title: "Equipment " + d.id, content: React.createElement("div", null,
+                        React.createElement("p", null, d.model),
+                        React.createElement("p", null, d.serialNumber),
+                        React.createElement("p", null, d.value),
+                        React.createElement("p", null, d.year)) }),
+                React.createElement("br", null));
+        })));
+    };
     scheduledEquipments.prototype.render = function () {
-        var i = 1;
-        var a = [];
-        while (i < 3) {
-            a.push(React.createElement("div", null,
-                React.createElement(Accordion_1.CustomAccordion, { title: "Equipment " + i.toString(), content: React.createElement("div", null,
-                        React.createElement("p", null, "Make"),
-                        React.createElement("p", null, "Model"),
-                        React.createElement("p", null, "Year"),
-                        React.createElement("p", null, "Serial Number"),
-                        React.createElement("p", null, "Value")) }),
-                React.createElement("br", null)));
-            i++;
-        }
         return (React.createElement(React.Fragment, null,
             React.createElement(NavMenu_1.default, null),
             React.createElement("div", { className: 'row' },
@@ -65,10 +76,10 @@ var scheduledEquipments = /** @class */ (function (_super) {
                         React.createElement("input", { type: 'submit', value: 'submit' })),
                     React.createElement("br", null),
                     React.createElement("br", null),
-                    a))));
+                    this.showEquipments()))));
     };
     return scheduledEquipments;
-}(React.Component));
+}(React.PureComponent));
 ;
-exports.default = (0, react_redux_1.connect)()(scheduledEquipments);
+exports.default = (0, react_redux_1.connect)(function (state) { return state.Equipments; }, EquipmentsStore.actionCreators)(scheduledEquipments);
 //# sourceMappingURL=scheduledEquipments.js.map
