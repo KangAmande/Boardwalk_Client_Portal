6a11fdad522d99be1244ce8758f301355f2b1749
Hruthvikk <101742067+Hruthvikk@users.noreply.github.com>
2022-04-09 18:07:36 -0400

    drivers makereq 4 working

--- WebApplication4/ClientApp/src/components/Make-Request.js
+++ WebApplication4/ClientApp/src/components/Make-Request.js
@@ -3,34 +3,68 @@ var __extends = (this && this.__extends) || (function () {
     var extendStatics = function (d, b) {
         extendStatics = Object.setPrototypeOf ||
             ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
         return extendStatics(d, b);
     };
     return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
         extendStatics(d, b);
         function __() { this.constructor = d; }
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 var React = require("react");
 var react_redux_1 = require("react-redux");
 var Sidebarmr_1 = require("./Sidebarmr");
 require("./form.css");
 var NavMenu_1 = require("./NavMenu");
+var CertificateRequestsStore = require("../store/CertificateRequests");
+var Accordion_1 = require("./Accordion");
 var MakeRequest = /** @class */ (function (_super) {
     __extends(MakeRequest, _super);
     function MakeRequest() {
         return _super !== null && _super.apply(this, arguments) || this;
     }
+    MakeRequest.prototype.componentDidMount = function () {
+        this.ensureDataFetched();
+    };
+    // This method is called when the route parameters change
+    MakeRequest.prototype.componentDidUpdate = function () {
+        this.ensureDataFetched();
+    };
+    MakeRequest.prototype.ensureDataFetched = function () {
+        var startDateIndex = parseInt(this.props.match.params.startDateIndex, 10) || 0;
+        this.props.requestCertificateRequests(startDateIndex);
+    };
+    MakeRequest.prototype.showCertificateRequests = function () {
+        console.log(this.props.CertificateRequests);
+        return (React.createElement("div", null, this.props.CertificateRequests.map(function (d, index) {
+            return React.createElement("div", null,
+                React.createElement(Accordion_1.CustomAccordion, { key: index, title: "Location", content: React.createElement("div", null,
+                        React.createElement("p", null,
+                            "Name : ",
+                            d.Name),
+                        React.createElement("p", null,
+                            "Address : ",
+                            d.Street),
+                        React.createElement("p", null,
+                            "Province : ",
+                            d.Province),
+                        React.createElement("p", null,
+                            "city : ",
+                            d.City),
+                        React.createElement("p", null,
+                            "Postal Code : ",
+                            d.PostalCode)) }),
+                React.createElement("br", null));
+        })));
+    };
     MakeRequest.prototype.render = function () {
         return (React.createElement(React.Fragment, null,
-            React.createElement(NavMenu_1.default, null),
+            React.createElement(NavMenu_1["default"], null),
             React.createElement("div", { className: 'row' },
                 React.createElement("div", { className: 'col-4' },
-                    React.createElement(Sidebarmr_1.default, null)),
+                    React.createElement(Sidebarmr_1["default"], null)),
                 React.createElement("div", { className: 'col-8', id: 'mr1add' },
                     React.createElement("h1", null, "Request Insurance Certificate"),
                     React.createElement("form", null,
@@ -70,10 +104,10 @@ var MakeRequest = /** @class */ (function (_super) {
                         React.createElement("br", null),
                         React.createElement("br", null),
                         React.createElement("input", { type: 'submit', value: 'submit' })),
-                    React.createElement("br", null)))));
+                    React.createElement("br", null),
+                    this.showCertificateRequests()))));
     };
     return MakeRequest;
-}(React.Component));
+}(React.PureComponent));
 ;
-exports.default = (0, react_redux_1.connect)()(MakeRequest);
-//# sourceMappingURL=Make-Request.js.map
+exports["default"] = react_redux_1.connect(function (state) { return state.CertificateRequests; }, CertificateRequestsStore.actionCreators)(MakeRequest);
--- WebApplication4/ClientApp/src/components/addDriver.js
+++ WebApplication4/ClientApp/src/components/addDriver.js
@@ -3,46 +3,58 @@ var __extends = (this && this.__extends) || (function () {
     var extendStatics = function (d, b) {
         extendStatics = Object.setPrototypeOf ||
             ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
         return extendStatics(d, b);
     };
     return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
         extendStatics(d, b);
         function __() { this.constructor = d; }
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 var React = require("react");
 var react_redux_1 = require("react-redux");
 var Accordion_1 = require("./Accordion");
 var Sidebarmr_1 = require("./Sidebarmr");
 var NavMenu_1 = require("./NavMenu");
+var DriversStore = require("../store/Drivers");
 var addDriver = /** @class */ (function (_super) {
     __extends(addDriver, _super);
     function addDriver() {
         return _super !== null && _super.apply(this, arguments) || this;
     }
+    addDriver.prototype.componentDidMount = function () {
+        this.ensureDataFetched();
+    };
+    // This method is called when the route parameters change
+    addDriver.prototype.componentDidUpdate = function () {
+        this.ensureDataFetched();
+    };
+    addDriver.prototype.ensureDataFetched = function () {
+        var startDateIndex = parseInt(this.props.match.params.startDateIndex, 10) || 0;
+        this.props.requestDrivers(startDateIndex);
+    };
+    addDriver.prototype.showDrivers = function () {
+        console.log(this.props.Driver);
+        return (React.createElement("div", null, this.props.Driver.map(function (d, index) {
+            return React.createElement("div", null,
+                React.createElement(Accordion_1.CustomAccordion, { key: index, title: "Location", content: React.createElement("div", null,
+                        React.createElement("p", null,
+                            "Full Name : ",
+                            d.fullName),
+                        React.createElement("p", null,
+                            "Driver License : ",
+                            d.driverLicense)) }),
+                React.createElement("br", null));
+        })));
+    };
     addDriver.prototype.render = function () {
-        var i = 1;
-        var a = [];
-        while (i < 3) {
-            a.push(React.createElement("div", null,
-                React.createElement(Accordion_1.CustomAccordion, { title: "drivers " + i.toString(), content: React.createElement("div", null,
-                        React.createElement("p", null, "Full Name"),
-                        React.createElement("p", null, "Birth Date"),
-                        React.createElement("p", null, "License Number"),
-                        React.createElement("p", null, "license Year")) }),
-                React.createElement("br", null)));
-            i++;
-        }
         return (React.createElement(React.Fragment, null,
-            React.createElement(NavMenu_1.default, null),
+            React.createElement(NavMenu_1["default"], null),
             React.createElement("div", { className: 'row' },
                 React.createElement("div", { className: 'col-4' },
-                    React.createElement(Sidebarmr_1.default, null)),
+                    React.createElement(Sidebarmr_1["default"], null)),
                 React.createElement("div", { className: 'col-8', id: 'mr2add' },
                     React.createElement("h1", null, "Add New Drivers"),
                     React.createElement("form", null,
@@ -70,10 +82,9 @@ var addDriver = /** @class */ (function (_super) {
                         React.createElement("input", { type: 'submit', value: 'submit' })),
                     React.createElement("br", null),
                     React.createElement("br", null),
-                    a))));
+                    this.showDrivers()))));
     };
     return addDriver;
-}(React.Component));
+}(React.PureComponent));
 ;
-exports.default = (0, react_redux_1.connect)()(addDriver);
-//# sourceMappingURL=addDriver.js.map
+exports["default"] = react_redux_1.connect(function (state) { return state.Drivers; }, DriversStore.actionCreators)(addDriver);
